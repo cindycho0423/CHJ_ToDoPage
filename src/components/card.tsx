a737f1cdd7205e617cc2e4eb99fa152bebf37f80
cindyCho <hjcho2303@naver.com>
2025-02-14 22:28:10 +0900

    design: 말 줄임표 적용

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -48,18 +48,15 @@ export default function Card({
   return (
     <div className="my-12 flex min-h-180 flex-col rounded-4 border border-solid bg-white/15 p-16">
       <div className="flex justify-between">
-        <div>
-          <span className="mr-12 text-24 font-semibold">{title}</span>
+        <div className="flex items-center">
+          <span className="mr-6 line-clamp-1 text-24 font-semibold">
+            {title}
+          </span>
           <Badge status={status} />
         </div>
         <Popover>
-          <Popover.Toggle>
-            <Image
-              src="/icons/ic_kebab.svg"
-              alt="kebab"
-              width={30}
-              height={30}
-            />
+          <Popover.Toggle className="relative size-30">
+            <Image src="/icons/ic_kebab.svg" alt="kebab" fill />
           </Popover.Toggle>
           <Popover.Wrapper>
             <Popover.Item onClick={handleEdit}>수정하기</Popover.Item>
